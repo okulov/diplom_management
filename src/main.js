02392b3d705bf9065626310598cbec664fd0809f
okulovvs <okulovvs@gmail.com>
2018-06-28 13:42:13 +0300

    init VueRouter

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
 import 'vuetify/dist/vuetify.css'
 
+Vue.use(VueRouter)
 Vue.use(Vuetify)
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
