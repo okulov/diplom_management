25801d3860be5f6ac488053c67182f177c632f42
Roland <okulovvs@gmail.com>
2018-06-29 07:44:25 +0300

    clear project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
-import VueRouter from 'vue-router'
+import router from './router'
 import 'vuetify/dist/vuetify.css'
 
-Vue.use(VueRouter)
 Vue.use(Vuetify)
 
 new Vue({
     el: '#app',
+    router,
     render: h => h(App)
 })
